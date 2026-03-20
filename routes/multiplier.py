9646242e29495546b99fa6f22bf677190020afc0
Meme29 <amelie.olemans.29@gmail.com>
2026-03-20 10:32:40 +0100

    5

--- routes/multiplier.py
+++ routes/multiplier.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get("/multiplier/{x}", tags=["Multiplier"])
-def get_products(x: int):
+def get_products(x: float):
     return {"result": x * 2}
